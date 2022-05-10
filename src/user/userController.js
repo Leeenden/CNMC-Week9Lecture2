33b22ad52f82448a877de909d4a3673342f348bd
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-10 11:45:35 +0100

    added user controller, modesl, routes

---
+++ src/user/userController.js
@@ -0,0 +1,14 @@
+const User = require("./userModel");
+
+// sign-up function
+exports.addUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).send({ user: newUser.username });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
+};
+
+//login function
